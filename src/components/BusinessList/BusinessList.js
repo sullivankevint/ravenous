d50c15a3000a42d02e484ac7784ab67e09099f82
Kevin Sullivan <sullivan.kevin.t@gmail.com>
2020-07-30 14:50:19 +0200

    Minor update to prevent crashing

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,17 +4,24 @@ import Business from '../Business/Business'
 
 class BusinessList extends React.Component {
   render() {
-    return (
-      <div className="BusinessList">
-        {
-          this.props.businesses.map((business) => {
-            return <Business
-              key={business.id}
-              business={business} />;
-          })
-        }
-      </div>
-    );
+    if(this.props.businesses) {
+      return (
+        <div className="BusinessList">
+          {
+            this.props.businesses.map((business) => {
+              return <Business
+                key={business.id}
+                business={business} />;
+            })
+          }
+        </div>
+      );
+    } else {
+      return (
+        <div className="BusinessList">
+        </div>
+      );
+    }
   }
 }
 
